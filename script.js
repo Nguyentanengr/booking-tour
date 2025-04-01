e91c5ff0945d65ae796ebac599c829bb2817f105
tan nguyen <nguyen.tan.engr@gmail.com>
2025-04-01 19:29:44 +0700

    feature | about us page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 50) { /* Khi cuộn quá 50px */
         navbar.classList.add('scrolled');
@@ -6,3 +6,20 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('scrolled');
     }
 });
+
+
+// slide comment feature
+const slide = document.querySelector(".review-slide")
+let index = 0;
+
+function nextRate() {
+    index++;
+    if (index >= 3) return;
+    slide.style.transform = `translateX(-${index * 100}%)`;
+}
+
+slide.addEventListener("transitionend", () => {
+   
+});
+
+setInterval(nextRate, 3000);
